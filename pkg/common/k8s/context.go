f3f88dd3c55e6867ff580f125eeab71b71c51719
Eric VanHeest <eric.van.heest@sap.com>
2020-06-16 16:23:53 -0600

    Errors in comments

--- pkg/common/k8s/context.go
+++ pkg/common/k8s/context.go
@@ -85,14 +85,14 @@ func InitializeTracing(logger *zap.SugaredLogger, ctx context.Context, service s
 		logger.Fatalw("Error getting kubernetes client from context (invalid interface)")
 	}
 
-	// Create A Watcher On The Logging ConfigMap & Dynamically Update Log Levels
+	// Create A Watcher On The Tracing ConfigMap & Dynamically Update Tracing Configuration
 	cmw := configmap.NewInformedWatcher(k8sClient, system.Namespace())
 
 	if err := tracing.SetupDynamicPublishing(logger, cmw, service, tracingconfig.ConfigName); err != nil {
 		logger.Fatalw("Error setting up dynamic trace publishing", zap.Error(err))
 	}
 
-	// Start The Logging ConfigMap Watcher
+	// Start The Tracing ConfigMap Watcher
 	if err := cmw.Start(ctx.Done()); err != nil {
 		logger.Fatalw("Failed To Start ConfigMap Watcher", zap.Error(err))
 	}
