f90c2348ccba875df014eda35231c0dba65d147a
eric-sap <eric.van.heest@sap.com>
2020-09-15 12:57:41 -0600

    Improve dispatcher naming (#55)
    * Added hash to channel and dispatcher deployment names
    
    * Hash combination of name/namespace instead of individually
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt Moore <mattmoor@vmware.com>
    
    * Moved GenerateHash to its own file
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt Moore <mattmoor@vmware.com>
    
    Co-authored-by: Matt Moore <mattmoor@vmware.com>

--- pkg/controller/util/dispatcher.go
+++ pkg/controller/util/dispatcher.go
@@ -2,16 +2,19 @@ package util
 
 import (
 	"fmt"
+
 	kafkav1beta1 "knative.dev/eventing-contrib/kafka/channel/pkg/apis/messaging/v1beta1"
 )
 
 // Create A DNS Safe Name For The Specified KafkaChannel Suitable For Use With K8S Services
 func DispatcherDnsSafeName(channel *kafkav1beta1.KafkaChannel) string {
 
-	// In order for the resulting name to be a valid DNS component is 63 characters.  We are appending 12 characters to separate
-	// the components and to indicate this is a Dispatcher which further reduces the available length to 51.  We will allocate 30
-	// characters to the channel and 20 to the namespace, leaving some extra buffer.
-	safeChannelName := GenerateValidDnsName(channel.Name, 30, true, false)
-	safeChannelNamespace := GenerateValidDnsName(channel.Namespace, 20, false, false)
-	return fmt.Sprintf("%s-%s-dispatcher", safeChannelName, safeChannelNamespace)
+	// In order for the resulting name to be a valid DNS component is 63 characters.  We are appending 13 characters to
+	// separate the components and to indicate this is a Dispatcher, and adding 8 hash characters, which further reduces
+	// the available length to 42.
+	// We will allocate 26 characters to the channel and 16 to the namespace, leaving some extra buffer.
+	safeChannelName := GenerateValidDnsName(channel.Name, 26, true, false)
+	safeChannelNamespace := GenerateValidDnsName(channel.Namespace, 16, false, false)
+	hash := GenerateHash(channel.Name+channel.Namespace, 8)
+	return fmt.Sprintf("%s-%s-%s-dispatcher", safeChannelName, safeChannelNamespace, hash)
 }
