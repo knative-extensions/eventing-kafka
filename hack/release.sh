b1acf1e8c830183c195ac75edb8bbc29525eef2a
Matthias Wessendorf <mwessend@redhat.com>
2020-10-06 16:42:21 +0200

    Respect that we have different things to release now (#82)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- hack/release.sh
+++ hack/release.sh
@@ -19,24 +19,36 @@
 
 source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/release.sh
 
+export GO111MODULE=on
+
+# Yaml files to generate, and the source config dir for them.
+declare -A COMPONENTS
+COMPONENTS=(
+  ["channel-consolidated.yaml"]="config/channel/consolidated"
+  ["channel-distributed.yaml"]="config/channel/distributed"
+  ["source.yaml"]="config/source"
+)
+readonly COMPONENTS
+
 function build_release() {
    # Update release labels if this is a tagged release
   if [[ -n "${TAG}" ]]; then
-    echo "Tagged release, updating release labels to kafka.eventing.knative.dev/release: \"${TAG}\""
-    LABEL_YAML_CMD=(sed -e "s|kafka.eventing.knative.dev/release: devel|kafka.eventing.knative.dev/release: \"${TAG}\"|")
+    echo "Tagged release, updating release labels to contrib.eventing.knative.dev/release: \"${TAG}\""
+    LABEL_YAML_CMD=(sed -e "s|contrib.eventing.knative.dev/release: devel|contrib.eventing.knative.dev/release: \"${TAG}\"|")
   else
     echo "Untagged release, will NOT update release labels"
     LABEL_YAML_CMD=(cat)
   fi
 
-  local config="config"
-  echo "Building eventing-kafka"
-
-
-  export CGO_ENABLED=1
-  ko resolve ${KO_FLAGS} -f ${config}/ | "${LABEL_YAML_CMD[@]}" > "eventing-kafka-channel.yaml"
-  ARTIFACTS_TO_PUBLISH="eventing-kafka-channel.yaml"
+  local all_yamls=()
+  for yaml in "${!COMPONENTS[@]}"; do
+    local config="${COMPONENTS[${yaml}]}"
+    echo "Building Knative Eventing Contrib - ${config}"
+    # TODO(chizhg): reenable --strict mode after https://github.com/knative/test-infra/issues/1262 is fixed.
+    ko resolve ${KO_FLAGS} -f ${config}/ | "${LABEL_YAML_CMD[@]}" > ${yaml}
+    all_yamls+=(${yaml})
+  done
+  ARTIFACTS_TO_PUBLISH="${all_yamls[@]}"
 }
 
 main $@
-
