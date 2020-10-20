b1730f261f1271fe1bb8d92938a1060fc430161d
I867318 <travis.minke@sap.com>
2020-10-20 11:45:40 -0600

    Add License To pkg/channel/distributed/controller

--- pkg/channel/distributed/controller/kafkachannel/reconciler_test.go
+++ pkg/channel/distributed/controller/kafkachannel/reconciler_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package kafkachannel
 
 import (
@@ -299,9 +315,9 @@ func TestReconcile(t *testing.T) {
 				controllertesting.NewKafkaChannelReceiverDeployment(),
 				controllertesting.NewKafkaChannelDispatcherDeployment(),
 			},
-			WithReactors:      []clientgotesting.ReactionFunc{InduceFailure("create", "services")},
-			WantErr:           true,
-			WantCreates:       []runtime.Object{controllertesting.NewKafkaChannelDispatcherService()},
+			WithReactors: []clientgotesting.ReactionFunc{InduceFailure("create", "services")},
+			WantErr:      true,
+			WantCreates:  []runtime.Object{controllertesting.NewKafkaChannelDispatcherService()},
 			WantStatusUpdates: []clientgotesting.UpdateActionImpl{
 				// Note - Not currently tracking status for the Dispatcher Service since it is only for Prometheus
 			},
