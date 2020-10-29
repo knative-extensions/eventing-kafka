c9f9515a9e95ab005a3cc53f80db2438e496ba5f
Eric VanHeest <eric.van.heest@sap.com>
2020-10-29 12:38:26 -0600

    Kill zipkin-forwarder between test runs

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -375,6 +375,10 @@ initialize $@ --skip-istio-addon "--gcp-project-id=${PROJECT_ID}"
 export SYSTEM_NAMESPACE
 
 test_consolidated_channel || return 1
+
+# Terminate any zipkin port-forward processes that are still present on the system
+ps -e -o pid,command | grep 'kubectl port-forward zipkin[^*]*9411:9411 -n' | cut -d ' ' -f 1 | xargs kill
+
 test_distributed_channel || return 1
 
 # If you wish to use this script just as test setup, *without* teardown, just uncomment this line and comment all go_test_e2e commands
