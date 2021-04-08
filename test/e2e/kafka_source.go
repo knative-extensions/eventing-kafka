056f3569486f46c85469fc465f204e59fbd816d6
Chris Suszyński <ksuszyns@redhat.com>
2021-04-08 20:43:43 +0200

    Fix codegen

--- test/e2e/kafka_source.go
+++ test/e2e/kafka_source.go
@@ -54,15 +54,190 @@ const (
 	kafkaTLSSecret  = "strimzi-tls-secret"
 )
 
+var (
+	testMtSource = os.Getenv("TEST_MT_SOURCE")
+)
+
+// SourceTestScope a func that will return true if we should proceed with given
+// test case.
+type SourceTestScope func(auth, testCase, version string) bool
+
+// AssureKafkaSourceIsOperational assures that KafkaSource works as intended.
+func AssureKafkaSourceIsOperational(t *testing.T, scope SourceTestScope) {
+	eventTime, _ := cetypes.ParseTime("2018-04-05T17:31:00Z")
+
+	auths := map[string]struct {
+		auth authSetup
+	}{
+		"plain": {
+			auth: authSetup{
+				bootStrapServer: kafkaBootstrapUrlPlain,
+				SASLEnabled:     false,
+				TLSEnabled:      false,
+			},
+		},
+		"s512": {
+			auth: authSetup{
+				bootStrapServer: kafkaBootstrapUrlSASL,
+				SASLEnabled:     true,
+				TLSEnabled:      false,
+			},
+		},
+		"tls": {
+			auth: authSetup{
+				bootStrapServer: kafkaBootstrapUrlTLS,
+				SASLEnabled:     false,
+				TLSEnabled:      true,
+			},
+		},
+	}
+	tests := map[string]struct {
+		messageKey     string
+		messageHeaders map[string]string
+		messagePayload string
+		matcherGen     func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher
+	}{
+		"no_event": {
+			messageKey: "0",
+			messageHeaders: map[string]string{
+				"content-type": "application/json",
+			},
+			messagePayload: `{"value":5}`,
+			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
+				return AllOf(
+					HasSource(cloudEventsSourceName),
+					HasType(cloudEventsEventType),
+					HasDataContentType("application/json"),
+					HasData([]byte(`{"value":5}`)),
+					HasExtension("key", "0"),
+				)
+			},
+		},
+		"no_event_no_content_type": {
+			messageKey:     "0",
+			messagePayload: `{"value":5}`,
+			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
+				return AllOf(
+					HasSource(cloudEventsSourceName),
+					HasType(cloudEventsEventType),
+					HasData([]byte(`{"value":5}`)),
+					HasExtension("key", "0"),
+				)
+			},
+		},
+		"no_event_content_type_or_key": {
+			messagePayload: `{"value":5}`,
+			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
+				return AllOf(
+					HasSource(cloudEventsSourceName),
+					HasType(cloudEventsEventType),
+					HasData([]byte(`{"value":5}`)),
+				)
+			},
+		},
+		"no_event_with_text_plain_body": {
+			messageKey: "0",
+			messageHeaders: map[string]string{
+				"content-type": "text/plain",
+			},
+			messagePayload: "simple 10",
+			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
+				return AllOf(
+					HasSource(cloudEventsSourceName),
+					HasType(cloudEventsEventType),
+					HasDataContentType("text/plain"),
+					HasData([]byte("simple 10")),
+					HasExtension("key", "0"),
+				)
+			},
+		},
+		"structured": {
+			messageHeaders: map[string]string{
+				"content-type": "application/cloudevents+json",
+			},
+			messagePayload: mustJsonMarshal(t, map[string]interface{}{
+				"specversion":     "1.0",
+				"type":            "com.github.pull.create",
+				"source":          "https://github.com/cloudevents/spec/pull",
+				"subject":         "123",
+				"id":              "A234-1234-1234",
+				"time":            "2018-04-05T17:31:00Z",
+				"datacontenttype": "application/json",
+				"data": map[string]string{
+					"hello": "Francesco",
+				},
+				"comexampleextension1": "value",
+				"comexampleothervalue": 5,
+			}),
+			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
+				return AllOf(
+					HasSpecVersion(cloudevents.VersionV1),
+					HasType("com.github.pull.create"),
+					HasSource("https://github.com/cloudevents/spec/pull"),
+					HasSubject("123"),
+					HasId("A234-1234-1234"),
+					HasTime(eventTime),
+					HasDataContentType("application/json"),
+					HasData([]byte(`{"hello":"Francesco"}`)),
+					HasExtension("comexampleextension1", "value"),
+					HasExtension("comexampleothervalue", "5"),
+				)
+			},
+		},
+		"binary": {
+			messageHeaders: map[string]string{
+				"ce_specversion":          "1.0",
+				"ce_type":                 "com.github.pull.create",
+				"ce_source":               "https://github.com/cloudevents/spec/pull",
+				"ce_subject":              "123",
+				"ce_id":                   "A234-1234-1234",
+				"ce_time":                 "2018-04-05T17:31:00Z",
+				"content-type":            "application/json",
+				"ce_comexampleextension1": "value",
+				"ce_comexampleothervalue": "5",
+			},
+			messagePayload: mustJsonMarshal(t, map[string]string{
+				"hello": "Francesco",
+			}),
+			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
+				return AllOf(
+					HasSpecVersion(cloudevents.VersionV1),
+					HasType("com.github.pull.create"),
+					HasSource("https://github.com/cloudevents/spec/pull"),
+					HasSubject("123"),
+					HasId("A234-1234-1234"),
+					HasTime(eventTime),
+					HasDataContentType("application/json"),
+					HasData([]byte(`{"hello":"Francesco"}`)),
+					HasExtension("comexampleextension1", "value"),
+					HasExtension("comexampleothervalue", "5"),
+				)
+			},
+		},
+	}
+	for authName, auth := range auths {
+		for testcase, test := range tests {
+			test := test
+			name := testcase + "_" + authName
+			for _, version := range []string{"v1alpha1", "v1beta1"} {
+				testName := name + "-" + version
+				if !scope(authName, testcase, version) {
+					t.Log("Skipping the test case, because it's out of configured scope: ", testName)
+					continue
+				}
+				t.Run(testName, func(t *testing.T) {
+					testKafkaSource(t, name, version, test.messageKey, test.messageHeaders, test.messagePayload, test.matcherGen, auth.auth)
+				})
+			}
+		}
+	}
+}
+
 type authSetup struct {
 	bootStrapServer string
 	SASLEnabled     bool
 	TLSEnabled      bool
 }
-
-var (
-	testMtSource = os.Getenv("TEST_MT_SOURCE")
-)
 
 func withAuthEnablementV1Beta1(auth authSetup) contribresources.KafkaSourceV1Beta1Option {
 	// We test with sasl512 and enable tls with it, so check tls first
@@ -253,181 +428,6 @@ func testKafkaSource(t *testing.T, name string, version string, messageKey strin
 	helpers.MustPublishKafkaMessage(client, kafkaBootstrapUrlPlain, kafkaTopicName, messageKey, messageHeaders, messagePayload)
 
 	eventTracker.AssertExact(1, recordevents.MatchEvent(matcherGen(cloudEventsSourceName, cloudEventsEventType)))
-}
-
-// SourceTestScope a func that will return true if we should proceed with given
-// test case.
-type SourceTestScope func(auth, testCase, version string) bool
-
-// AssureKafkaSourceIsOperational assures that KafkaSource works as intended.
-func AssureKafkaSourceIsOperational(t *testing.T, scope SourceTestScope) {
-	eventTime, _ := cetypes.ParseTime("2018-04-05T17:31:00Z")
-
-	auths := map[string]struct {
-		auth authSetup
-	}{
-		"plain": {
-			auth: authSetup{
-				bootStrapServer: kafkaBootstrapUrlPlain,
-				SASLEnabled:     false,
-				TLSEnabled:      false,
-			},
-		},
-		"s512": {
-			auth: authSetup{
-				bootStrapServer: kafkaBootstrapUrlSASL,
-				SASLEnabled:     true,
-				TLSEnabled:      false,
-			},
-		},
-		"tls": {
-			auth: authSetup{
-				bootStrapServer: kafkaBootstrapUrlTLS,
-				SASLEnabled:     false,
-				TLSEnabled:      true,
-			},
-		},
-	}
-	tests := map[string]struct {
-		messageKey     string
-		messageHeaders map[string]string
-		messagePayload string
-		matcherGen     func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher
-	}{
-		"no_event": {
-			messageKey: "0",
-			messageHeaders: map[string]string{
-				"content-type": "application/json",
-			},
-			messagePayload: `{"value":5}`,
-			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
-				return AllOf(
-					HasSource(cloudEventsSourceName),
-					HasType(cloudEventsEventType),
-					HasDataContentType("application/json"),
-					HasData([]byte(`{"value":5}`)),
-					HasExtension("key", "0"),
-				)
-			},
-		},
-		"no_event_no_content_type": {
-			messageKey:     "0",
-			messagePayload: `{"value":5}`,
-			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
-				return AllOf(
-					HasSource(cloudEventsSourceName),
-					HasType(cloudEventsEventType),
-					HasData([]byte(`{"value":5}`)),
-					HasExtension("key", "0"),
-				)
-			},
-		},
-		"no_event_content_type_or_key": {
-			messagePayload: `{"value":5}`,
-			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
-				return AllOf(
-					HasSource(cloudEventsSourceName),
-					HasType(cloudEventsEventType),
-					HasData([]byte(`{"value":5}`)),
-				)
-			},
-		},
-		"no_event_with_text_plain_body": {
-			messageKey: "0",
-			messageHeaders: map[string]string{
-				"content-type": "text/plain",
-			},
-			messagePayload: "simple 10",
-			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
-				return AllOf(
-					HasSource(cloudEventsSourceName),
-					HasType(cloudEventsEventType),
-					HasDataContentType("text/plain"),
-					HasData([]byte("simple 10")),
-					HasExtension("key", "0"),
-				)
-			},
-		},
-		"structured": {
-			messageHeaders: map[string]string{
-				"content-type": "application/cloudevents+json",
-			},
-			messagePayload: mustJsonMarshal(t, map[string]interface{}{
-				"specversion":     "1.0",
-				"type":            "com.github.pull.create",
-				"source":          "https://github.com/cloudevents/spec/pull",
-				"subject":         "123",
-				"id":              "A234-1234-1234",
-				"time":            "2018-04-05T17:31:00Z",
-				"datacontenttype": "application/json",
-				"data": map[string]string{
-					"hello": "Francesco",
-				},
-				"comexampleextension1": "value",
-				"comexampleothervalue": 5,
-			}),
-			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
-				return AllOf(
-					HasSpecVersion(cloudevents.VersionV1),
-					HasType("com.github.pull.create"),
-					HasSource("https://github.com/cloudevents/spec/pull"),
-					HasSubject("123"),
-					HasId("A234-1234-1234"),
-					HasTime(eventTime),
-					HasDataContentType("application/json"),
-					HasData([]byte(`{"hello":"Francesco"}`)),
-					HasExtension("comexampleextension1", "value"),
-					HasExtension("comexampleothervalue", "5"),
-				)
-			},
-		},
-		"binary": {
-			messageHeaders: map[string]string{
-				"ce_specversion":          "1.0",
-				"ce_type":                 "com.github.pull.create",
-				"ce_source":               "https://github.com/cloudevents/spec/pull",
-				"ce_subject":              "123",
-				"ce_id":                   "A234-1234-1234",
-				"ce_time":                 "2018-04-05T17:31:00Z",
-				"content-type":            "application/json",
-				"ce_comexampleextension1": "value",
-				"ce_comexampleothervalue": "5",
-			},
-			messagePayload: mustJsonMarshal(t, map[string]string{
-				"hello": "Francesco",
-			}),
-			matcherGen: func(cloudEventsSourceName, cloudEventsEventType string) EventMatcher {
-				return AllOf(
-					HasSpecVersion(cloudevents.VersionV1),
-					HasType("com.github.pull.create"),
-					HasSource("https://github.com/cloudevents/spec/pull"),
-					HasSubject("123"),
-					HasId("A234-1234-1234"),
-					HasTime(eventTime),
-					HasDataContentType("application/json"),
-					HasData([]byte(`{"hello":"Francesco"}`)),
-					HasExtension("comexampleextension1", "value"),
-					HasExtension("comexampleothervalue", "5"),
-				)
-			},
-		},
-	}
-	for authName, auth := range auths {
-		for testcase, test := range tests {
-			test := test
-			name := testcase + "_" + authName
-			for _, version := range []string{"v1alpha1", "v1beta1"} {
-				testName := name + "-" + version
-				if !scope(authName, testcase, version) {
-					t.Log("Skipping the test case, because it's out of configured scope: ", testName)
-					continue
-				}
-				t.Run(testName, func(t *testing.T) {
-					testKafkaSource(t, name, version, test.messageKey, test.messageHeaders, test.messagePayload, test.matcherGen, auth.auth)
-				})
-			}
-		}
-	}
 }
 
 func mustJsonMarshal(t *testing.T, val interface{}) string {
