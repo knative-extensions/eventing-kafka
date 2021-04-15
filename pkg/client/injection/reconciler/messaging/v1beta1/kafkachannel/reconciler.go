44f87d52ecacd977cc373e4a5058c96cb3f39fa3
Francesco Guardiani <francescoguard@gmail.com>
2021-04-15 13:44:02 +0200

    Update to latest eventing (#532)
    * Update
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Fix issue + force binary to avoid regression
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Nit
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * More update
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    * Update codegen
    
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- pkg/client/injection/reconciler/messaging/v1beta1/kafkachannel/reconciler.go
+++ pkg/client/injection/reconciler/messaging/v1beta1/kafkachannel/reconciler.go
@@ -88,13 +88,13 @@ type doReconcile func(ctx context.Context, o *v1beta1.KafkaChannel) reconciler.E
 
 // reconcilerImpl implements controller.Reconciler for v1beta1.KafkaChannel resources.
 type reconcilerImpl struct {
-	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware
+	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware.
 	reconciler.LeaderAwareFuncs
 
 	// Client is used to write back status updates.
 	Client versioned.Interface
 
-	// Listers index properties about resources
+	// Listers index properties about resources.
 	Lister messagingv1beta1.KafkaChannelLister
 
 	// Recorder is an event recorder for recording Event resources to the
@@ -116,7 +116,7 @@ type reconcilerImpl struct {
 	skipStatusUpdates bool
 }
 
-// Check that our Reconciler implements controller.Reconciler
+// Check that our Reconciler implements controller.Reconciler.
 var _ controller.Reconciler = (*reconcilerImpl)(nil)
 
 // Check that our generated Reconciler is always LeaderAware.
--- pkg/source/adapter/message.go
+++ pkg/source/adapter/message.go
@@ -28,15 +28,13 @@ import (
 	protocolkafka "github.com/cloudevents/sdk-go/protocol/kafka_sarama/v2"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/binding"
-	"github.com/cloudevents/sdk-go/v2/extensions"
 	"github.com/cloudevents/sdk-go/v2/protocol/http"
-	"go.opencensus.io/trace"
 	"go.uber.org/zap"
 
 	sourcesv1beta1 "knative.dev/eventing-kafka/pkg/apis/sources/v1beta1"
 )
 
-func (a *Adapter) ConsumerMessageToHttpRequest(ctx context.Context, span *trace.Span, cm *sarama.ConsumerMessage, req *nethttp.Request) error {
+func (a *Adapter) ConsumerMessageToHttpRequest(ctx context.Context, cm *sarama.ConsumerMessage, req *nethttp.Request) error {
 	msg := protocolkafka.NewMessageFromConsumerMessage(cm)
 
 	defer func() {
@@ -46,12 +44,9 @@ func (a *Adapter) ConsumerMessageToHttpRequest(ctx context.Context, span *trace.
 		}
 	}()
 
-	// Build tracing ext to write it as output
-	tracingExt := extensions.FromSpanContext(span.SpanContext())
-
 	if msg.ReadEncoding() != binding.EncodingUnknown {
 		// Message is a CloudEvent -> Encode directly to HTTP
-		return http.WriteRequest(ctx, msg, req, tracingExt.WriteTransformer())
+		return http.WriteRequest(cloudevents.WithEncodingBinary(ctx), msg, req)
 	}
 
 	a.logger.Debug("Message is not a CloudEvent -> We need to translate it to a valid CloudEvent")
@@ -67,12 +62,17 @@ func (a *Adapter) ConsumerMessageToHttpRequest(ctx context.Context, span *trace.
 
 	dumpKafkaMetaToEvent(&event, a.keyTypeMapper, cm.Key, kafkaMsg)
 
-	err := event.SetData(kafkaMsg.ContentType, kafkaMsg.Value)
-	if err != nil {
-		return err
+	if kafkaMsg.ContentType == "" {
+		// This avoids base64 encoding when sending as json structured
+		event.DataEncoded = kafkaMsg.Value
+	} else {
+		err := event.SetData(kafkaMsg.ContentType, kafkaMsg.Value)
+		if err != nil {
+			return err
+		}
 	}
 
-	return http.WriteRequest(ctx, binding.ToMessage(&event), req, tracingExt.WriteTransformer())
+	return http.WriteRequest(ctx, binding.ToMessage(&event), req)
 }
 
 func makeEventId(partition int32, offset int64) string {
