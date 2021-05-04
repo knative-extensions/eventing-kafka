b44e5212bbad246b2dd2edd5ec275dd211bf7f41
I867318 <travis.minke@sap.com>
2021-05-04 09:33:15 -0600

    goimports and update-codegen

--- pkg/apis/kafka/v1alpha1/reset_offset_types.go
+++ pkg/apis/kafka/v1alpha1/reset_offset_types.go
@@ -17,9 +17,9 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"github.com/Shopify/sarama"
 	"time"
 
+	"github.com/Shopify/sarama"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
