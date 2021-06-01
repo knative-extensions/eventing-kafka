7b4abe0f7b6b244ba2fa8994ed21eba6f8cce2e2
Eric VanHeest <eric.van.heest@sap.com>
2021-06-01 17:26:51 -0600

    Added test coverage for moved code

--- pkg/common/config/util_test.go
+++ pkg/common/config/util_test.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"testing"
 
-	"github.com/Shopify/sarama"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/eventing-kafka/pkg/common/constants"
+	"knative.dev/pkg/system"
+
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
@@ -80,6 +83,11 @@ func TestGetAuthConfigFromKubernetes(t *testing.T) {
 			askName: commontesting.SecretName,
 		},
 		{
+			name:    "Valid secret, backwards-compatibility",
+			secret:  oldAuthSecret,
+			askName: commontesting.SecretName,
+		},
+		{
 			name:      "Valid secret, not found",
 			secret:    oldAuthSecret,
 			askName:   "invalid-secret-name",
@@ -105,22 +113,103 @@ func TestGetAuthConfigFromKubernetes(t *testing.T) {
 	}
 }
 
-func TestGetConfigFromSecret_Valid(t *testing.T) {
-	secret := getSaramaTestSecret(t,
-		commontesting.SecretName,
-		commontesting.OldAuthUsername,
-		commontesting.OldAuthPassword,
-		commontesting.OldAuthNamespace,
-		"")
-	kafkaAuth := GetAuthConfigFromSecret(secret)
-	assert.Equal(t, commontesting.OldAuthUsername, kafkaAuth.SASL.User)
-	assert.Equal(t, commontesting.OldAuthPassword, kafkaAuth.SASL.Password)
-	assert.Equal(t, sarama.SASLTypePlaintext, kafkaAuth.SASL.SaslType)
-}
+func TestGetAuthConfigFromSecret(t *testing.T) {
+	// Setup Test Environment Namespaces
+	commontesting.SetTestEnvironment(t)
 
-func TestGetConfigFromSecret_Invalid(t *testing.T) {
-	authCfg := GetAuthConfigFromSecret(nil)
-	assert.Nil(t, authCfg)
+	// Define The TestCase Struct
+	type TestCase struct {
+		name      string
+		data      map[string][]byte
+		expectNil bool
+	}
+
+	// Asserts that, if there is a key in the data, that its mapped value is the same as the given value
+	assertKey := func(t *testing.T, data map[string][]byte, key string, value string) {
+		mapValue, ok := data[key]
+		if !ok {
+			return
+		}
+		assert.Equal(t, string(mapValue), value)
+	}
+
+	// Create The TestCases
+	testCases := []TestCase{
+		{
+			name: "Valid secret",
+			data: map[string][]byte{
+				constants.KafkaSecretKeyUsername: []byte(commontesting.OldAuthUsername),
+				constants.KafkaSecretKeyPassword: []byte(commontesting.OldAuthPassword),
+				constants.KafkaSecretKeySaslType: []byte(commontesting.OldAuthSaslType),
+			},
+		},
+		{
+			name: "Valid secret, backwards-compatibility, TLS and SASL",
+			data: map[string][]byte{
+				TlsCacert:    []byte("test-cacert"),
+				TlsUsercert:  []byte("test-usercert"),
+				TlsUserkey:   []byte("test-userkey"),
+				TlsEnabled:   []byte("true"),
+				SaslUser:     []byte("test-sasluser"),
+				SaslPassword: []byte("test-password"),
+			},
+		},
+		{
+			name: "Valid secret, backwards-compatibility, TLS/SASL without CA cert",
+			data: map[string][]byte{
+				TlsEnabled:   []byte("true"),
+				SaslUser:     []byte("test-sasluser"),
+				SaslPassword: []byte("test-password"),
+			},
+		},
+		{
+			name: "Valid secret, backwards-compatibility, SASL, Invalid TLS",
+			data: map[string][]byte{
+				TlsEnabled:   []byte("invalid-bool"),
+				SaslUser:     []byte("test-sasluser"),
+				SaslPassword: []byte("test-password"),
+			},
+		},
+		{
+			name:      "Valid secret, backwards-compatibility, nil data",
+			data:      nil,
+			expectNil: true,
+		},
+	}
+
+	// Run The TestCases
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			secret := &corev1.Secret{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Secret",
+					APIVersion: corev1.SchemeGroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      commontesting.SecretName,
+					Namespace: system.Namespace(),
+				},
+				Data: testCase.data,
+			}
+			kafkaAuth := GetAuthConfigFromSecret(secret)
+			if testCase.expectNil {
+				assert.Nil(t, kafkaAuth)
+			} else {
+				assert.NotNil(t, kafkaAuth)
+				assert.NotNil(t, kafkaAuth.SASL)
+				assertKey(t, testCase.data, constants.KafkaSecretKeyUsername, kafkaAuth.SASL.User)
+				assertKey(t, testCase.data, constants.KafkaSecretKeyPassword, kafkaAuth.SASL.Password)
+				assertKey(t, testCase.data, constants.KafkaSecretKeySaslType, kafkaAuth.SASL.SaslType)
+				assertKey(t, testCase.data, SaslUser, kafkaAuth.SASL.User)
+				assertKey(t, testCase.data, SaslPassword, kafkaAuth.SASL.Password)
+				if kafkaAuth.TLS != nil {
+					assertKey(t, testCase.data, TlsCacert, kafkaAuth.TLS.Cacert)
+					assertKey(t, testCase.data, TlsUsercert, kafkaAuth.TLS.Usercert)
+					assertKey(t, testCase.data, TlsUserkey, kafkaAuth.TLS.Userkey)
+				}
+			}
+		})
+	}
 }
 
 func getSaramaTestSecret(t *testing.T, name string,
