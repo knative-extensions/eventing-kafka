d640c8e238947d6992633748912c285321308562
Stavros Kontopoulos <skontopo@redhat.com>
2021-06-16 17:23:49 +0300

    fix lint

--- pkg/source/adapter/adapter.go
+++ pkg/source/adapter/adapter.go
@@ -67,10 +67,10 @@ func NewEnvConfig() adapter.EnvConfigAccessor {
 }
 
 type Adapter struct {
-	config            *AdapterConfig
-	controlServer     *ctrlnetwork.ControlServer
-	saramaConfig      *sarama.Config
-	offsetInitialized bool
+	config        *AdapterConfig
+	controlServer *ctrlnetwork.ControlServer
+	saramaConfig  *sarama.Config
+	// offsetInitialized bool
 
 	httpMessageSender *kncloudevents.HTTPMessageSender
 	reporter          pkgsource.StatsReporter
