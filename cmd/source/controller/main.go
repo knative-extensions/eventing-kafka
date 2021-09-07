e67a45463a73e7ec71bfc271f592ea1f74cb24bd
Lionel Villard <villard@us.ibm.com>
2021-09-07 16:37:46 -0400

    add Kafka defaults, starting with KEDA annotations (#855)
    Co-authored-by: Steven DOng <steven.dong@ibm.com>

--- cmd/source/controller/main.go
+++ cmd/source/controller/main.go
@@ -20,28 +20,29 @@ import (
 	"context"
 	"os"
 
-	"knative.dev/eventing-kafka/pkg/apis/bindings"
-	"knative.dev/eventing-kafka/pkg/apis/sources"
-	"knative.dev/pkg/webhook/resourcesemantics/conversion"
-
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
-	bindingsv1beta1 "knative.dev/eventing-kafka/pkg/apis/bindings/v1beta1"
-	sourcesv1beta1 "knative.dev/eventing-kafka/pkg/apis/sources/v1beta1"
-
-	"knative.dev/eventing-kafka/pkg/source/reconciler/binding"
-	"knative.dev/eventing-kafka/pkg/source/reconciler/source"
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/injection"
 	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/logging"
 	"knative.dev/pkg/signals"
 	"knative.dev/pkg/webhook"
 	"knative.dev/pkg/webhook/certificates"
 	"knative.dev/pkg/webhook/psbinding"
 	"knative.dev/pkg/webhook/resourcesemantics"
+	"knative.dev/pkg/webhook/resourcesemantics/conversion"
 	"knative.dev/pkg/webhook/resourcesemantics/defaulting"
 	"knative.dev/pkg/webhook/resourcesemantics/validation"
+
+	"knative.dev/eventing-kafka/pkg/apis/bindings"
+	bindingsv1beta1 "knative.dev/eventing-kafka/pkg/apis/bindings/v1beta1"
+	"knative.dev/eventing-kafka/pkg/apis/sources"
+	kafkasourcedefaultconfig "knative.dev/eventing-kafka/pkg/apis/sources/config"
+	sourcesv1beta1 "knative.dev/eventing-kafka/pkg/apis/sources/v1beta1"
+	"knative.dev/eventing-kafka/pkg/source/reconciler/binding"
+	"knative.dev/eventing-kafka/pkg/source/reconciler/source"
 )
 
 const (
@@ -57,6 +58,15 @@ var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
 var callbacks = map[schema.GroupVersionKind]validation.Callback{}
 
 func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	// Decorate contexts with the current state of the config.
+	kafkaStore := kafkasourcedefaultconfig.NewStore(logging.FromContext(ctx).Named("kafka-source-config-store"))
+	kafkaStore.WatchConfigs(cmw)
+
+	// Decorate contexts with the current state of the config.
+	ctxFunc := func(ctx context.Context) context.Context {
+		return kafkaStore.ToContext(ctx)
+	}
+
 	return defaulting.NewAdmissionController(ctx,
 
 		// Name of the resource webhook.
@@ -69,11 +79,7 @@ func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher
 		types,
 
 		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
-		func(ctx context.Context) context.Context {
-			// Here is where you would infuse the context with state
-			// (e.g. attach a store with configmap data)
-			return ctx
-		},
+		ctxFunc,
 
 		// Whether to disallow unknown fields.
 		true,
---
+++ pkg/apis/sources/config/store_test.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2021 The Knative Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"k8s.io/apimachinery/pkg/api/resource"
+	logtesting "knative.dev/pkg/logging/testing"
+
+	. "knative.dev/pkg/configmap/testing"
+)
+
+var ignoreStuff = cmp.Options{
+	cmpopts.IgnoreUnexported(resource.Quantity{}),
+}
+
+func TestStoreLoadWithContext(t *testing.T) {
+	store := NewStore(logtesting.TestLogger(t))
+
+	_, defaultsConfig := ConfigMapsFromTestFile(t, KafkaDefaultsConfigName)
+
+	store.OnConfigChanged(defaultsConfig)
+
+	config := FromContextOrDefaults(store.ToContext(context.Background()))
+
+	t.Run("defaults", func(t *testing.T) {
+		expected, _ := NewKafkaDefaultsConfigFromConfigMap(defaultsConfig)
+		if diff := cmp.Diff(expected, config.KafkaSourceDefaults, ignoreStuff...); diff != "" {
+			t.Error("Unexpected defaults config (-want, +got):", diff)
+			t.Fatal("Unexpected defaults config (-want, +got):", diff)
+		}
+	})
+}
+
+func TestStoreLoadWithContextOrDefaults(t *testing.T) {
+	defaultsConfig := ConfigMapFromTestFile(t, KafkaDefaultsConfigName)
+	config := FromContextOrDefaults(context.Background())
+
+	t.Run("defaults", func(t *testing.T) {
+		expected, _ := NewKafkaDefaultsConfigFromConfigMap(defaultsConfig)
+		if diff := cmp.Diff(expected, config.KafkaSourceDefaults, ignoreStuff...); diff != "" {
+			t.Error("Unexpected defaults config (-want, +got):", diff)
+		}
+	})
+}
