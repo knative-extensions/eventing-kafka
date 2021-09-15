344bfa43d16e708e0b3abd58a5293997a7a6f38e
Matthias Wessendorf <mwessend@redhat.com>
2021-09-15 17:56:09 +0200

    Updating to Strimzi 0.25 version (#866)
    * Updatint to Strimzi 0.25 version
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>
    
    * Remove Authz on users, since it was never enabled on the broker before
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -73,7 +73,7 @@ readonly EVENTING_KAFKA_SECRET_TEMPLATE="300-kafka-secret.yaml"
 readonly EVENTING_KAFKA_CONFIG_TEMPLATE="300-eventing-kafka-configmap.yaml"
 
 # Strimzi installation config template used for starting up Kafka clusters.
-readonly STRIMZI_INSTALLATION_CONFIG_TEMPLATE="test/config/100-strimzi-cluster-operator-0.23.0.yaml"
+readonly STRIMZI_INSTALLATION_CONFIG_TEMPLATE="test/config/100-strimzi-cluster-operator-0.25.0.yaml"
 # Strimzi installation config.
 readonly STRIMZI_INSTALLATION_CONFIG="$(mktemp "${ARTIFACTS}/strimzi-XXXXX.yaml")"
 # Kafka cluster CR config file.
@@ -83,7 +83,7 @@ readonly KAFKA_TLS_CONFIG="test/config/config-kafka-tls.yaml"
 # Kafka SASL ConfigMap.
 readonly KAFKA_SASL_CONFIG="test/config/config-kafka-sasl.yaml"
 # Kafka Users CR config file.
-readonly KAFKA_USERS_CONFIG="test/config/100-strimzi-users-0.23.0.yaml"
+readonly KAFKA_USERS_CONFIG="test/config/100-strimzi-users.yaml"
 # Kafka PLAIN cluster URL
 readonly KAFKA_PLAIN_CLUSTER_URL="my-cluster-kafka-bootstrap.kafka.svc.cluster.local:9092"
 # Kafka TLS cluster URL
