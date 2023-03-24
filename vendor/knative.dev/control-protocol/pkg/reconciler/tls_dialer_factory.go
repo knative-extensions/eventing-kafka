17056dcbf2820a0285baf6f78d49299b9347fbd1
knative-automation <automation@knative.team>
2023-03-24 02:13:54 -0400

    upgrade to latest dependencies (#1318)
    bumping knative.dev/control-protocol e5eadf2...33b1cf0:
      > 33b1cf0 Adding namespace to SAN  (# 265)
      > a273cac upgrade to latest dependencies (# 268)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/control-protocol/pkg/reconciler/tls_dialer_factory.go
+++ vendor/knative.dev/control-protocol/pkg/reconciler/tls_dialer_factory.go
@@ -70,7 +70,7 @@ func (ch *ListerCertificateGetter) GenerateTLSDialer(baseDialOptions *net.Dialer
 	tlsConfig := &tls.Config{
 		Certificates: []tls.Certificate{controlPlaneCert},
 		RootCAs:      certPool,
-		ServerName:   certificates.FakeDnsName,
+		ServerName:   certificates.DataPlaneNamePrefix + ch.namespace,
 	}
 
 	// Copy from base dial options
