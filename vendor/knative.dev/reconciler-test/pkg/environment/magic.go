370d63e1e5ad0608794ee9951615566a3c62c91a
Knative Automation <automation@knative.team>
2023-05-11 02:02:34 +0000

    upgrade to latest dependencies
    bumping knative.dev/eventing 9cdea5d...a9908ef:%0A  > a9908ef Support TLS in PingSource (# 6929)%0A  > df559c0 Fix typo in flags.IsDisbledTransportEncryption name (# 6941)%0A  > 7073cc9 [main] Upgrade to latest dependencies (# 6939)%0A  > c6bc9bb Eventing TLS: Support K_CA_CERTS env variable injection for SinkBinding subjects (# 6931)%0A  > 24fbfe5 Eventing TLS: support exposing https address in Broker controller (# 6930)%0A  > d18cb42 Add information about retryable error in servermanager (# 6921)%0A  > f92a05b Added Support for K_CA_CERTS in the heartbeats (# 6920)%0A  > b8b43d0 Remove CA certs empty and non nil check, use URL scheme only (# 6928)%0A  > 3c8cc05 Return error directly if one receiver of servermanager fails (# 6919)%0A  > 92ab7f8 [main] Upgrade to latest dependencies (# 6927)%0A  > 5c6fe57 two more for reducing to debug, instead of info (# 6922)%0A  > 6cf9397 less verbose logs on scheduler component  (# 6912)%0A  > 69918f2 Adds ServerManager. Supports http/https message receivers (# 6908)%0A  > d58e259 Install ko using setup-ko in kind e2e tests (# 6910)%0Abumping knative.dev/reconciler-test dc48d52...914f5d7:%0A  > 914f5d7 Add support for feature-level prerequisites (# 523)%0A  > 8a5db1b Remove resources when terminating the environment (# 519)%0A  > aced4ce Remove dependency on prow (# 520)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/reconciler-test/pkg/environment/magic.go
+++ vendor/knative.dev/reconciler-test/pkg/environment/magic.go
@@ -18,6 +18,7 @@ package environment
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"regexp"
 	"sync"
@@ -128,6 +129,11 @@ func (mr *MagicEnvironment) Finish() {
 	var result milestone.Result = unknownResult{}
 	if mr.managedT != nil {
 		result = mr.managedT
+		if !result.Failed() {
+			if err := feature.DeleteResources(mr.c, mr.managedT, mr.References()); err != nil {
+				mr.managedT.Fatal(err)
+			}
+		}
 	}
 	if mr.milestones != nil {
 		mr.milestones.Finished(result)
@@ -358,8 +364,9 @@ func (mr *MagicEnvironment) test(ctx context.Context, originalT *testing.T, f *f
 	// skip is flag that signals whether the steps for the subsequent timings should
 	// be skipped because a step in a previous timing failed.
 	//
-	// Setup and Teardown steps are executed always
+	// Setup and Teardown steps are executed always except when a Prerequisite step failed.
 	skip := false
+	skipTeardown := false
 
 	originalT.Run(f.Name, func(t *testing.T) {
 
@@ -381,10 +388,14 @@ func (mr *MagicEnvironment) test(ctx context.Context, originalT *testing.T, f *f
 						steps = mr.loggingSteps()
 					}
 				}
-				skip = false
+				skip = skipTeardown
 			}
 
 			originalT.Logf("Running %d steps for timing:\n%s\n\n", len(steps), steps.String())
+
+			// aggregator aggregates steps results (success or failure) for a single timing.
+			// It used for handling the prerequisite logic.
+			aggregator := newStepExecutionAggregator()
 
 			t.Run(timing.String(), func(t *testing.T) {
 				// no parallel, various timing steps run in order: setup, requirement, assert, teardown
@@ -396,13 +407,26 @@ func (mr *MagicEnvironment) test(ctx context.Context, originalT *testing.T, f *f
 
 				for _, s := range steps {
 					s := s
-					if mr.shouldFail(&s) {
-						mr.execute(ctx, t, f, &s)
+					if mr.shouldFail(&s) && timing != feature.Prerequisite {
+						mr.execute(ctx, t, f, &s, aggregator)
 					} else {
-						mr.executeOptional(ctx, t, f, &s)
+						mr.executeOptional(ctx, t, f, &s, aggregator)
 					}
 				}
 			})
+
+			// If any step at timing feature.Prerequisite failed, we should skip the feature.
+			if timing == feature.Prerequisite {
+				failed := aggregator.Failed()
+				if len(failed) > 0 {
+					bytes, _ := json.MarshalIndent(failed, "", "  ")
+					originalT.Logf("Prerequisite steps failed, skipping the remaining timings and steps, failed prerequisite steps:\n%s\n", string(bytes))
+
+					// Skip any other subsequent timing.
+					skip = true
+					skipTeardown = true
+				}
+			}
 
 			if t.Failed() {
 				// skip the following timings since curring timing failed
